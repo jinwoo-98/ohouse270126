3a1dbdfe878f8231baa95257c5526d24edea376a
[dyad] <tranvu20398@gmail.com>
2026-02-23 14:34:08 +0700

    [dyad] Tối ưu SEO Trang chủ và hiệu năng hình ảnh ProductCard/HeroSlider - wrote 4 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,26 +7,36 @@ import { FlashSale } from "@/components/home/FlashSale";
 import { ShopTheLook } from "@/components/home/ShopTheLook";
 import { FeaturedProducts } from "@/components/home/FeaturedProducts";
 import { TrendingSearch } from "@/components/home/TrendingSearch";
+import { Helmet } from "react-helmet-async";
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      
-      <main className="flex-1">
-        <HeroSlider />
-        <div className="space-y-0"> {/* Wrapper to manage vertical flow */}
-          <USPBar />
-          <CategoryGrid />
-          <FlashSale />
-          <ShopTheLook />
-          <FeaturedProducts />
-          <TrendingSearch />
-        </div>
-      </main>
+    <>
+      <Helmet>
+        <title>OHOUSE - Nội Thất Cao Cấp | Sang Trọng & Hiện Đại</title>
+        <meta name="description" content="OHOUSE - Thương hiệu nội thất cao cấp hàng đầu Việt Nam. Mang đến không gian sống sang trọng, hiện đại với hàng nghìn sản phẩm đa dạng từ sofa, bàn ăn, giường ngủ đến đèn trang trí." />
+        <meta property="og:title" content="OHOUSE - Nội Thất Cao Cấp | Sang Trọng & Hiện Đại" />
+        <meta property="og:description" content="Thương hiệu nội thất cao cấp hàng đầu Việt Nam. Kiến tạo không gian sống đẳng cấp." />
+        <meta property="og:type" content="website" />
+      </Helmet>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        
+        <main className="flex-1">
+          <HeroSlider />
+          <div className="space-y-0"> {/* Wrapper to manage vertical flow */}
+            <USPBar />
+            <CategoryGrid />
+            <FlashSale />
+            <ShopTheLook />
+            <FeaturedProducts />
+            <TrendingSearch />
+          </div>
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
